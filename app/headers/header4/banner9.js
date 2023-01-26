6d6c404cea16fcd5a257a01dc286168325a0af77
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-01-26 15:46:49 +0200

    banners

---
+++ app/headers/header4/banner9.js
@@ -0,0 +1,29 @@
+function bannerInfo1() {
+  mangaInfo.innerHTML = `
+  <h1>Tomo-chan wa Onna no ko!</h1>
+  <p>Artists: Yanagida Fumita</p>
+  <p>Authors: Yanagida Fumita</p>
+  <p>Status: Completed</p>
+  <p>Ranked: #437</p>
+
+  <p>
+    Genres: 4-Koma, Comedy, Romance, School Life, Slice of Life,
+    Sports
+  </p>
+  <h2>Description</h2>
+  <p>
+    Tomo Aizawa was able to successfully confess to her crush
+    Junichirou Kubota. <br />
+    There is only one problem: He just sees her as his buddy. He
+    thought that she was a boy until they went to middle school
+    together, because her grade school was in a different school
+    district.
+  </p>`;
+}
+
+function header1() {
+  header.classList.remove("header2");
+  header.classList.remove("header3");
+  header.classList.remove("header4");
+  header.classList.add("header1");
+}
