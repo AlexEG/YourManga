7c0289b35f0c5b57a2cbfc64e82b9bf907e45d84
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-04-18 11:30:01 +0200

    fix bug (github pages) #2

--- src/JS/header.js
+++ src/JS/header.js
@@ -9,22 +9,54 @@ const CoversContainer = document.querySelector(
   "header #mangaValumesCoversContainer"
 );
 
+// //  DEV
+// // ---------------------------------------------------- //
+
+// let mangaBannerCount = 1;
+// function autoChangeBannerImg() {
+//   setTimeout(function () {
+//     header.style.backgroundImage = `url('.${document
+//       .querySelector(
+//         `#mangaValumesCoversContainer > img:nth-child(${mangaBannerCount})`
+//       )
+//       .src.slice(21)}')`;
+
+//     mangaCover.src = `.${document
+//       .querySelector(
+//         ` #mangaValumesCoversContainer > img:nth-child(${mangaBannerCount})`
+//       )
+//       .src.slice(21)}`;
+
+//     console.log(
+//       `%c ${bannerMangaTitle.textContent}  %c${mangaBannerCount}`,
+//       `color:#ff009975`,
+//       `color:red; background-color:#55555575; padding-inline:4px; border-radius:8px`
+//     );
+
+//     mangaBannerCount++;
+//     autoChangeBannerImg();
+//   }, 1000);
+//   // ------
+//   if (mangaBannerCount > CoversContainer.childElementCount) {
+//     mangaBannerCount = 1;
+//   }
+// }
+// autoChangeBannerImg();
+// // ---------------------------------------------------- //
+
+//  GITHUB Pages
 // ---------------------------------------------------- //
 
 let mangaBannerCount = 1;
 function autoChangeBannerImg() {
   setTimeout(function () {
-    header.style.backgroundImage = `url('.${document
-      .querySelector(
-        `#mangaValumesCoversContainer > img:nth-child(${mangaBannerCount})`
-      )
-      .src.slice(21)}')`;
+    header.style.backgroundImage = `url('.${document.querySelector(
+      `#mangaValumesCoversContainer > img:nth-child(${mangaBannerCount})`
+    )}')`;
 
-    mangaCover.src = `.${document
-      .querySelector(
-        ` #mangaValumesCoversContainer > img:nth-child(${mangaBannerCount})`
-      )
-      .src.slice(21)}`;
+    mangaCover.src = `.${document.querySelector(
+      ` #mangaValumesCoversContainer > img:nth-child(${mangaBannerCount})`
+    )}`;
 
     console.log(
       `%c ${bannerMangaTitle.textContent}  %c${mangaBannerCount}`,
